308c30c2e3e9265378e7ca1d90220d3effe45822
FarIzad <Izadyar_Farnoosh2003@yahoo.com>
2023-06-18 23:32:13 +0330

    CommunicationHandler Class Created.

---
+++ src/CommunicationHandler.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.*;
+
+public class CommunicationHandler extends Thread {
+    private final Socket socket;
+
+    private ArrayList<Thread> threads;
+    private static long socketCount;
+
+    private final long socketID;
+
+    public CommunicationHandler(Socket socket, ArrayList<Thread> threads) {
+        this.socket = socket;
+        socketID = ++socketCount;
+        this.threads = threads;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader bufferIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String message;
+            //---------------
+            while (true) {
+                message = bufferIn.readLine();
+                if (message.equals("Exit")) break;
+                else {
+                    System.out.println("Received message from client: " + message);
+                    for (Thread thread : threads) {
+                        if (thread != Thread.currentThread()) {
+                            PrintWriter PWriter = new PrintWriter(socket.getOutputStream(), true);
+                            PWriter.println(message);
+                        }
+                    }
+                    System.out.printf("Server %d: %s\n" ,socketID , message);
+                    System.out.println(this.socketID + " : " + message);
+                }
+            }
+            System.out.println("Client Disconnected...");
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Waiting...");
+        ArrayList<Thread> usersThreads = new ArrayList<>();
+        try (ServerSocket server = new ServerSocket(4000)) {
+            while(true)
+            {
+                Socket clientSocket = server.accept();
+                System.out.println("User Connected!");
+                //----------
+                CommunicationHandler handler = new CommunicationHandler(clientSocket,usersThreads);
+                handler.start();
+            }
+        } catch (IOException IO) {
+            System.out.println("Error in Connection!");
+        }
     }
 }
