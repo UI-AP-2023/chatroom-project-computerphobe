3e45b9a029ed56b7cfeeb8647d9365b45f8fb716
Metiran <iranpourmahdi19@yahoo.com>
2023-06-20 13:29:14 +0330

    ping sender for client added

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) {
         try {
+
             Socket clientA = new Socket("localhost",4000);
             new ReadThread(clientA).start();
             new WriteThread(clientA).start();
+
         } catch (IOException ex) {
+
             System.out.println("I/O Error: " + ex.getMessage());
         }
     }
--- src/ReadThread.java
+++ src/ReadThread.java
@@ -4,24 +4,43 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class ReadThread extends Thread{
-    private String name = "User A";
 
-    private Socket client;
+    private final Socket clientSocket;
 
-    public ReadThread(Socket client) {
-        this.client = client;
+    public ReadThread(Socket clientSocket) {
+        this.clientSocket = clientSocket;
     }
 
     @Override
     public void run() {
+
         try {
-            System.out.println(this.name + " Connected To The Server On Port " + client.getPort());
-            BufferedReader bufferIn = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            String name = "User A";
+            System.out.println(name + " Connected To The Server On Port " + clientSocket.getPort());
+            BufferedReader bufferIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            //receive server current time and calculate ping
+            {
+                long now = System.currentTimeMillis();
+
+                long serverCurrentTime = Long.parseLong(bufferIn.readLine());
+
+                long ping = now - serverCurrentTime;
+
+                System.out.println("Ping: "+ ping + " milli second");
+            }
+
+            //=----------------------------------
+
+
             while (true) {
+
                 String incomingMessage = bufferIn.readLine();
                 if (incomingMessage.equals("Finish")) break;
                 System.out.println(incomingMessage);
             }
+
         } catch (IOException IO) {
             System.out.println("Error in Connection");
         }
